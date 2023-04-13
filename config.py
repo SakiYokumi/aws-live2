0d2bb7e6df1caaec7cc63221340c3c5a873471a6
SakiYokumi <125659723+SakiYokumi@users.noreply.github.com>
2023-04-13 21:45:15 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "database-1.ce2yuxclramc.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "lowchoonkeat-employee1"
+customregion = "us-east-1"
